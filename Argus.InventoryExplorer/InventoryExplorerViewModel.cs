278c0d1d451ca1aef9a2e7c8ceeefc2608a988f6
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-05-07 16:19:42 -0600

    updated the application to use MVVM architecture as opposed to just codebehind

---
+++ Argus.InventoryExplorer/InventoryExplorerViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Argus.InventoryExplorer.Infrastructure;
+using Argus.InventoryExplorer.Models;
+
+namespace Argus.InventoryExplorer.ViewModels
+{
+    public sealed class InventoryExplorerViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<InventoryItem> InventoryItems { get; } =
+            new ObservableCollection<InventoryItem>();
+
+        private InventoryItem? _selectedItem;
+        public InventoryItem? SelectedItem
+        {
+            get => _selectedItem;
+            set { _selectedItem = value; OnPropertyChanged(); }
+        }
+
+        public ICommand RefreshCommand { get; }
+        public ICommand AddItemCommand { get; }
+        public ICommand RemoveCommand { get; }
+
+        public InventoryExplorerViewModel()
+        {
+            RefreshCommand = new RelayCommand(_ => LoadData());
+            AddItemCommand = new RelayCommand(_ => AddNewItem());
+            RemoveCommand = new RelayCommand(
+                                 _ => RemoveSelected(),
+                                 _ => SelectedItem != null);
+
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            InventoryItems.Clear();
+            InventoryItems.Add(new InventoryItem { Id = 1, Name = "Widget", Quantity = 25 });
+            InventoryItems.Add(new InventoryItem { Id = 2, Name = "Gadget", Quantity = 10 });
+            InventoryItems.Add(new InventoryItem { Id = 3, Name = "Doohickey", Quantity = 5 });
+        }
+
+        void AddNewItem()
+        {
+            var id = InventoryItems.Count + 1;
+            InventoryItems.Add(new InventoryItem
+            {
+                Id = id,
+                Name = $"New Item {id}",
+                Quantity = 0
+            });
+        }
+
+        void RemoveSelected()
+        {
+            if (SelectedItem != null)
+                InventoryItems.Remove(SelectedItem);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+            if (name == nameof(SelectedItem))
+                ((RelayCommand)RemoveCommand).RaiseCanExecuteChanged();
+        }
+    }
+}
--- Argus.InventoryExplorer/MainWindow.xaml.cs
+++ Argus.InventoryExplorer/MainWindow.xaml.cs
@@ -1,40 +1,9 @@
-﻿using System.Collections.ObjectModel;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Argus.InventoryExplorer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = this;
-        }
-
-        public class InventoryItem
-        {
-            public int Id { get; set; }
-            public string Name { get; set; } = "";
-            public int Quantity { get; set; }
-        }
-
-        public ObservableCollection<InventoryItem> InventoryItems { get; } =
-            new ObservableCollection<InventoryItem>
-            {
-                new InventoryItem { Id = 1, Name = "Widget", Quantity = 25 },
-                new InventoryItem { Id = 2, Name = "Gadget", Quantity = 10 }
-            };
-        }
+        public MainWindow() => InitializeComponent();
+    }
 }
